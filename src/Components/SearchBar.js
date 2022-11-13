19b34ddeb082f691cb70f9d02deb06a64611327a
Stephanie Li <listeph15@gmail.com>
2022-11-12 23:18:01 -0600

    added state to search

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -5,6 +5,12 @@ import {useState} from 'react';
 // receives props from database 
 const SearchBar = (props) => {
 
+    const [searchInput, setSearchInput] = useState("");
+
+    const handleChange = (e) => {
+        setSearchInput(e.target.value);
+    }
+
 
     return ( 
         <div className="search-wrapper">
@@ -12,9 +18,14 @@ const SearchBar = (props) => {
                 <input 
                     type="text"
                     placeHolder="Enter a meal..."
+                    value={searchInput}
+                    onChange={handleChange}
                  />
-
-
+            </div>
+            <div>
+                <div className="search-results">
+                    {}
+                </div>
             </div>
 
         </div> 
